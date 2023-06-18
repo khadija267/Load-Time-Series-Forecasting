cdbf5f0c81162a13aa9bc2ef2a7721d182548723
Khadija Hesham <37675645+khadija267@users.noreply.github.com>
2023-06-18 19:08:50 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -14,3 +14,16 @@ def LLR(model1,model2,deg_free=1):
     p_value=chi2.sf(LR,deg_free).round(3)
     return p_value
 
+def training_loop(n_epochs, optimiser, model, loss_fn, X_train,  X_val, y_train, y_val):
+    for epoch in range(1, n_epochs + 1):
+        output_train = model(X_train) # forwards pass
+        loss_train = loss_fn(output_train, y_train) # calculate loss
+        output_val = model(X_val) 
+        loss_val = loss_fn(output_val, y_val)
+        
+        optimiser.zero_grad() # set gradients to zero
+        loss_train.backward() # backwards pass
+        optimiser.step() # update model parameters
+        if epoch == 1 or epoch % 10000 == 0:
+            print(f"Epoch {epoch}, Training loss {loss_train.item():.4f},"
+                  f" Validation loss {loss_val.item():.4f}")
